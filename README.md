c2f30470c94d371ed919f71375a46e7e59548bd2
saycoron <saycoron@gmail.com>
2023-10-26 18:50:08 +0300

    Create bot, add bot token in project. First run bot.

---
+++ README.md
@@ -0,0 +1,5 @@
+Whisper Telegram bot
+
+Powered by  
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)  
+![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)
---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+load_dotenv()
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=os.getenv('BOT_TOKEN', 'BOT_TOKEN'))
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    await message.answer('Hello!')
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
